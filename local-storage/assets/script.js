e7d15e0e349c6ae88f0fea3402908f0f82fc99b9
Metin-3 <7kp0v0e@code.edu.az>
2023-02-14 21:51:35 +0400

    to do list - local storage

---
+++ local-storage/assets/script.js
@@ -0,0 +1,59 @@
+const inputVal = document.getElementsByClassName("inputVal")[0];
+const addTaskBtn = document.getElementsByClassName("btn")[0];
+
+
+addTaskBtn.addEventListener("click", () => {
+
+    if (inputVal.value.trim() != 0) {
+        let localItems = JSON.parse(localStorage.getItem('localItem'));
+
+        if (localItems === null) {
+            taskList = [];
+        } else {
+            taskList = localItems;
+        }
+
+        taskList.push(inputVal.value);
+        localStorage.setItem('localItem', JSON.stringify(taskList));
+    }
+
+    showlist();
+});
+
+function showlist() {
+    let outPut = '';
+    let taskListShow = document.querySelector(".totoListItem")
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+
+    if (localItems === null) {
+        taskList = [];
+    } else {
+        taskList = localItems;
+    }
+
+    taskList.forEach((data, index) => {
+        outPut += `
+        <div class="todoList">
+            <p class="pText">${data}</p>
+            <button class="deleteTask" onclick="deleteItem(${index})">x</button>
+        </div>
+        `
+    });
+
+    taskListShow.innerHTML = outPut;
+}
+
+showlist()
+
+
+function deleteItem(index) {
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+    taskList.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(taskList))
+    showlist()
+}
+
+function clearTask() {
+    localStorage.clear()
+    showlist()
+}
